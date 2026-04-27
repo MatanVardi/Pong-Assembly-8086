a11339cce9fee84296fad3f874fc2fb9fa512af2
MatanV <matanvardi1@gmail.com>
2026-04-27 14:48:06 +0300

    Drew the left and the right palletes, in the middle of making them move

--- main.asm
+++ main.asm
@@ -1,10 +1,20 @@
 .model small
 .stack 100h
 
-; TODO: 1. open a graphics screen, 2. draw paddle, 3. make it mode 4. draw a ball and add physics.  5. draw and move another paddle.  add score.
+; TODO: 3. make it mode 4. draw a ball and add physics.  5. draw and move another paddle.  add score.
 .data
-	leftPadY dw 120
-	leftPadX dw 30
+	leftPadY dw 80
+	leftPadX dw 10
+	paddleWidth dw 07h
+	paddleHeight dw 30
+	rightPadY dw 80
+	rightPadX dw 280
+	
+	topBoundary dw 10
+	downBoundary dw 310
+	velocity_y dw 20
+
+	
 
 
 
@@ -18,39 +28,62 @@ setGraphic proc
 	ret
 setGraphic endp
 
-drawPixel proc
+
+x equ [bp+4]
+y equ [bp+6]
+drawPaddle proc
 	push bp
+	mov bp, sp
+
 	push dx
 	push cx
 	push bx 
 	push ax
-	mov bp, sp
-	mov     dx, [leftPadY]  ; y value  
-	mov     cx, [leftPadX]  ; x value 
-	mov     bh, 0      
-	mov     ax, 0C02h  
-	int     10h
+	
+	
+	mov cx, x ; X 
+	mov dx, y ; Y
+	
+	draw_paddle_left_horizontal:
+		mov ah, 0Ch
+		mov al, 0Fh
+		mov bh, 00h
+		int 10h
+		
+		inc cx
+		mov ax, cx
+		sub ax, x
+		cmp ax, paddleWidth
+		jng draw_paddle_left_horizontal
+		
+		mov cx, x
+		inc dx
+		mov ax, dx
+		sub ax, y
+		cmp ax, paddleHeight
+		jng draw_paddle_left_horizontal
 	pop bp
 	pop ax
 	pop bx
 	pop cx
 	pop dx
-	ret
-drawPixel endp
+	ret 4
+drawPaddle endp
 
-clear_screen proc
+clearScreenGraphics proc
 	push ax
-	mov ah,00h 						
-	mov al,13h 						
-	int 10h							
-
-	mov ah,0Bh						
-	mov bh,00h						
-	mov bl,00h 						
-	int 10h
+    mov ax, 0A000h     
+    mov es, ax
+    xor di, di         
+    mov al, 0          
+    mov cx, 64000      
+    cld                
+    rep stosb          
 	pop ax
-	ret
-clear_screen endp
+    ret
+clearScreenGraphics endp
+
+
 
 
 
@@ -75,6 +108,113 @@ checkExit proc
 	ret
 checkExit endp
 
+checkPaddleLeftMove proc
+	push ax
+	push dx
+	push cx
+	
+	mov ah, 00h
+	int 16h
+	cmp al, 'w'
+	je move_up_check1
+	cmp al, 's'
+	je move_down_check1
+	
+	move_up_check1:
+		mov dx, [topBoundary]
+		cmp [leftPadY], dx
+		jg move_up1
+		jmp ending1
+	move_up1:
+		mov cx, [velocity_y]
+		sub [leftPadY], cx
+		call clearScreenGraphics
+		push [leftPadY]
+		push [leftPadX]
+		call drawPaddle
+		push [rightPadY]
+		push [rightPadX]
+		call drawPaddle
+
+		jmp ending1
+	move_down_check1:
+		mov dx, [downBoundary]
+		cmp [leftPadY], dx
+		jl move_down1 
+		jmp ending1
+	move_down1:
+		mov cx, [velocity_y]
+		add [leftPadY], cx
+		call clearScreenGraphics
+		push [leftPadY]
+		push [leftPadX]
+		call drawPaddle
+		push [rightPadY]
+		push [rightPadX]
+		call drawPaddle
+
+		jmp ending1
+
+	ending1:
+		pop cx
+		pop dx
+		pop ax
+		ret
+checkPaddleLeftMove endp
+
+checkPaddleRightMove proc
+	push ax
+	push dx
+	push cx
+	
+	mov ah, 00h
+	int 16h
+	cmp al, 'i'
+	je move_up_check2
+	cmp al, 'k'
+	je move_down_check2
+	
+	move_up_check2:
+		mov dx, [topBoundary]
+		cmp [rightPadY], dx
+		jg move_up2
+		jmp ending2
+	move_up2:
+		mov cx, [velocity_y]
+		sub [rightPadY], cx
+		call clearScreenGraphics
+		push [rightPadY]
+		push [rightPadX]
+		call drawPaddle
+		push [leftPadY]
+		push [leftPadX]
+		call drawPaddle
+
+		jmp ending2
+	move_down_check2:
+		mov dx, [downBoundary]
+		cmp [rightPadY], dx
+		jl move_down2
+		jmp ending2
+	move_down2:
+		mov cx, [velocity_y]
+		add [rightPadY], cx
+		call clearScreenGraphics
+		push [rightPadY]
+		push [rightPadX]
+		call drawPaddle
+		push [leftPadY]
+		push [leftPadX]
+		call drawPaddle
+
+		jmp ending2
+
+	ending2:
+		pop cx
+		pop dx
+		pop ax
+		ret
+checkPaddleRightMove endp
 
 main proc 
 
@@ -82,13 +222,22 @@ main proc
 	mov ds, ax
 	
 	call setGraphic
-	;call drawPaddle
-	
-	start_loop:	
-		call drawPixel
+	push [leftPadY]
+	push [leftPadX]
+	call drawPaddle
+		
+	push [rightPadY]
+	push [rightPadX]
+	call drawPaddle
 
+
+	start_loop:	
+		
 		call checkExit
-		;call clear_screen
+		call checkPaddleLeftMove
+		call checkPaddleRightMove
+
+		
 		jmp start_loop
 		
 main endp 
